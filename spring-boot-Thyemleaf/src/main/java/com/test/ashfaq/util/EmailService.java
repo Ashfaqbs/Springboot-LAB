984d173f9581edf4033f6b7f35bd2e7b93811660
Ashfaq <105435085+Ashfaqbs@users.noreply.github.com>
2024-04-20 19:26:56 +0530

    Receive Mail Sample

--- spring-boot-Thyemleaf/src/main/java/com/test/ashfaq/util/EmailService.java
+++ spring-boot-Thyemleaf/src/main/java/com/test/ashfaq/util/EmailService.java
@@ -1,6 +1,13 @@
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+
 
 @Service
 public class EmailService {
@@ -25,6 +32,47 @@ public class EmailService {
         message.setSubject(subject);
         message.setText(text);
         emailSender.send(message);
+    }
+
+
+
+     @Value("${spring.mail.username}")
+    private String from;
+
+    public void receiveMail() {
+        // Setup your email server properties
+        Properties properties = new Properties();
+        properties.setProperty("mail.store.protocol", "imap");
+        properties.setProperty("mail.imap.host", "your_imap_host");
+        properties.setProperty("mail.imap.port", "your_imap_port");
+
+        // Connect to the email server
+        Session session = Session.getDefaultInstance(properties);
+        try {
+            Store store = session.getStore();
+            store.connect("your_email", "your_password");
+
+            // Open the inbox folder
+            Folder inbox = store.getFolder("INBOX");
+            inbox.open(Folder.READ_ONLY);
+
+            // Fetch messages from the inbox
+            Message[] messages = inbox.getMessages();
+            for (Message message : messages) {
+                // Process each message
+                System.out.println("Subject: " + message.getSubject());
+                System.out.println("From: " + message.getFrom()[0]);
+                System.out.println("Body: " + message.getContent().toString());
+
+                // You can implement your logic here to handle the email
+            }
+
+            // Close the inbox
+            inbox.close(true);
+            store.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
