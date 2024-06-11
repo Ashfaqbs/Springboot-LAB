c5aa58c2f0a1d7f2ead12d9506cd2d5a04362987
Ashfaq <105435085+Ashfaqbs@users.noreply.github.com>
2024-06-11 07:40:16 +0530

    opencsv reader demo

---
+++ Snippets-SB3-J21/src/main/java/com/ashfaq/dev/snips/Util/Record.java
@@ -0,0 +1,83 @@
+package com.ashfaq.dev.ai;
+
+public class Record {
+    private int primaryKey;
+    private String name;
+    private String age;
+    private String city;
+    private String remarks;
+    private String other;
+
+
+    
+
+    public Record(int primaryKey, String name, String age, String city, String remarks, String other) {
+		this.primaryKey = primaryKey;
+		this.name = name;
+		this.age = age;
+		this.city = city;
+		this.remarks = remarks;
+		this.other = other;
+	}
+
+    
+    
+    
+    
+    
+    
+	public String getOther() {
+		return other;
+	}
+
+
+
+
+
+
+
+	public void setOther(String other) {
+		this.other = other;
+	}
+
+
+
+
+
+
+
+	public int getPrimaryKey() {
+        return primaryKey;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+
+
+
+
+
+
+	@Override
+	public String toString() {
+		return "Record [primaryKey=" + primaryKey + ", name=" + name + ", age=" + age + ", city=" + city + ", remarks="
+				+ remarks + ", other=" + other + "]";
+	}
+
+    
+}
+
