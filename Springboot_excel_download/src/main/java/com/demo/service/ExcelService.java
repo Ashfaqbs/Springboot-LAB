da75a1a0c9baf64f14b1f00f8bfa3fc7d188e45d
Ashfaq <105435085+Ashfaqbs@users.noreply.github.com>
2024-06-20 18:56:32 +0530

    Update service

---
+++ Springboot_excel_download/src/main/java/com/demo/service/ExcelService.java
@@ -0,0 +1,37 @@
+package com.example.excelupload.service;
+
+import org.apache.poi.ss.usermodel.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
+
+@Service
+public class ExcelService {
+
+    public void readExcel(MultipartFile file) throws Exception {
+        try (InputStream inputStream = file.getInputStream()) {
+            Workbook workbook = WorkbookFactory.create(inputStream);
+            Sheet sheet = workbook.getSheetAt(0);
+            for (Row row : sheet) {
+                for (Cell cell : row) {
+                    switch (cell.getCellType()) {
+                        case STRING:
+                            System.out.print(cell.getStringCellValue() + "\t");
+                            break;
+                        case NUMERIC:
+                            System.out.print(cell.getNumericCellValue() + "\t");
+                            break;
+                        case BOOLEAN:
+                            System.out.print(cell.getBooleanCellValue() + "\t");
+                            break;
+                        default:
+                            System.out.print("Unknown Cell Type" + "\t");
+                            break;
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
+}
