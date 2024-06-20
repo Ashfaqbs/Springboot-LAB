f26bacd9c2c6fb6b800b46af26e3d9976c5fbcab
Ashfaq <105435085+Ashfaqbs@users.noreply.github.com>
2024-06-20 19:27:18 +0530

    employee parse from excel

--- Springboot_excel_download/src/main/java/com/demo/service/ExcelService.java
+++ Springboot_excel_download/src/main/java/com/demo/service/ExcelService.java
@@ -9,6 +9,44 @@ import java.io.InputStream;
 @Service
 public class ExcelService {
 
+
+   public List<Employee> readExcel(MultipartFile file) throws Exception {
+        List<Employee> employees = new ArrayList<>();
+
+        try (InputStream inputStream = file.getInputStream()) {
+            Workbook workbook = WorkbookFactory.create(inputStream);
+            Sheet sheet = workbook.getSheetAt(0);
+
+            for (Row row : sheet) {
+                if (row.getRowNum() == 0) { // Skip header row
+                    continue;
+                }
+
+                Employee employee = new Employee();
+                for (Cell cell : row) {
+                    switch (cell.getColumnIndex()) {
+                        case 0:
+                            employee.setName(cell.getStringCellValue());
+                            break;
+                        case 1:
+                            employee.setAge((int) cell.getNumericCellValue());
+                            break;
+                        case 2:
+                            employee.setDepartment(cell.getStringCellValue());
+                            break;
+                        case 3:
+                            employee.setSalary(cell.getNumericCellValue());
+                            break;
+                    }
+                }
+                employees.add(employee);
+            }
+        }
+        return employees;
+    }
+
+
+
     public void readExcel(MultipartFile file) throws Exception {
         try (InputStream inputStream = file.getInputStream()) {
             Workbook workbook = WorkbookFactory.create(inputStream);
