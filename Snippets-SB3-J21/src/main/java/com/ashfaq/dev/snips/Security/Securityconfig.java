e12f6298210a0d16fc9dae1f9a0e21ebef0c8ef8
Ashfaq <105435085+Ashfaqbs@users.noreply.github.com>
2024-07-18 20:49:12 +0530

    Update Securityconfig.java

--- Snippets-SB3-J21/src/main/java/com/ashfaq/dev/snips/Security/Securityconfig.java
+++ Snippets-SB3-J21/src/main/java/com/ashfaq/dev/snips/Security/Securityconfig.java
@@ -4,18 +4,23 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer { // no required to implements WebMvcConfigurer for below but try it our 
 
+// CORS Config 
+    
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://your-frontend-domain.com") // Replace with your frontend domain
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                  registry.addMapping("/**")  // Apply CORS settings to all paths
+                        .allowedOrigins("http://my-frontend-domain.com")  // Replace with frontend domain
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // HTTP methods allowed
+                        .allowedHeaders("*")  // Allow all headers
+                        // .allowCredentials(true)  // Allow credentials like cookies and authorization headers if we provide this we cant provide multiple origin
+                          .maxAge(3600);  // Cache preflight response for 1 hour (3600 seconds)
+                         // maxAge(3600): Sets the maximum age (in seconds) for which the results of a preflight request can be cached by the client.  it's set to 1 hour (3600 seconds).
+                //its for HTTP option
             }
         };
     }
