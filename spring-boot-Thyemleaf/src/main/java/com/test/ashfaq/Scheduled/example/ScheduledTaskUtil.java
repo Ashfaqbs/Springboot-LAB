11d8573baba69310da41714e75978aa683c0039a
Ashfaq <105435085+Ashfaqbs@users.noreply.github.com>
2024-11-07 18:06:09 +0530

    Create ScheduledTaskUtil.java

---
+++ spring-boot-Thyemleaf/src/main/java/com/test/ashfaq/Scheduled/example/ScheduledTaskUtil.java
@@ -0,0 +1,23 @@
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledTaskUtil {
+
+	// Inject the cron expression from the properties file
+	@Value("${app.schedule.cron}")
+	private String cronExpression;
+
+	// Use the cron expression in the @Scheduled annotation
+	@Scheduled(cron = "${app.schedule.cron}")
+	public void executeScheduledTask() {
+		System.out.println("Scheduled task is running with cron expression: " + cronExpression);
+	}
+}
+
+
+// application prop file 
+// # Runs every minute
+// app.schedule.cron=* * * * * *
